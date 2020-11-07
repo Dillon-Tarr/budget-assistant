a0e4f6eefe3bb8feb2592fc59ebc6957eb884e93
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-11-07 00:16:57 -0500

    Added timeline; updated Mongoose models

--- server/models/budget.js
+++ server/models/budget.js
@@ -1,14 +1,42 @@
 const mongoose = require('mongoose');
 
+const incomeSchema = new mongoose.Schema({
+  name: { type: String, required: true, min: 1},
+  startDate: { type: Date, required: true }, // specified to the year, month, and day
+  dollarsPerOccurrence: { type: Number, required: true },
+  isRecurring: { type: Boolean, required: true },
+  inclusiveEndDate: { type: Date, required: false }, // specified to the year, month, and day
+  referencePeriod: { type: String, required: false }, // day, week, month, or year
+  multiplesOfPeriod: { type: Number, required: false }, // e.g. 3 (along with month) for the period to be every three months
+  weekOfMonthText: { type: String, required: false }, // first, second, third, fourth, last
+  daysOfWeek: {type: Array, required: false }, // Contains strings (e.g. "Monday", "Thursday") for when occurrences are on specific days of the week.
+  daysOfMonth: {type: Array, required: false }, // Contains numbers for when occurrences are on specific number days of the month.
+});
+const outgoSchema = new mongoose.Schema({
+  name: { type: String, required: true, min: 1},
+  startDate: { type: Date, required: true }, // specified to the year, month, and day
+  dollarsPerOccurrence: { type: Number, required: true },
+  doRemind: { type: Boolean, required: true, default: false },
+  remindThisManyDaysBefore: { type: Number, required: false },
+  muteRemindersUntil: { type: Date, required: false },
+  isRecurring: { type: Boolean, required: true },
+  inclusiveEndDate: { type: Date, required: false }, // specified to the year, month, and day
+  referencePeriod: { type: String, required: false }, // day, week, month, or year
+  multiplesOfPeriod: { type: Number, required: false }, // e.g. 3 (along with month) for the period to be every three months
+  weekOfMonthText: { type: String, required: false }, // first, second, third, fourth, last
+  daysOfWeek: {type: Array, required: false }, // Contains strings (e.g. "Monday", "Thursday") for when occurrences are on specific days of the week.
+  daysOfMonth: {type: Array, required: false }, // Contains numbers for when occurrences are on specific number days of the month.
+});
+
 const budgetSchema = new mongoose.Schema({
   createdDate: { type: Date, default: Date.now },
-  managers: {type: Array, required: true, unique: false },
-  viewers: {type: Array, required: false, unique: false },
-  income: {type: Array, required: false, unique: false },
-  outgo: {type: Array, required: false, unique: false },
-  lastChangedDate: { type: Date, required: false, unique: false},
-  recentChanges: { type: Array, required: false, unique: false },
-  requestedChanges: { type: Array, required: false, unique: false }
+  managers: {type: Array, required: true },
+  viewers: {type: Array, required: false },
+  income: {type: [incomeSchema], required: false },
+  outgo: {type: [outgoSchema], required: false },
+  lastChangedDate: { type: Date, required: false },
+  recentChanges: { type: Array, required: false },
+  requestedChanges: { type: Array, required: false }
 });
 
 const Budget = mongoose.model('Budget', budgetSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
+const goalSchema = new mongoose.Schema({
+  text: { type: String, required: true, unique: false, minlength: 1, maxlength: 255 },
+  isComplete: { type: Boolean, required: true, unique: false, default: false },
+  estimatedCompletionDate: { type: Date, required: false, unique: false },
+  completedDate: { type: Date, required: false, unique: false }
+});
+
 const userSchema = new mongoose.Schema({
   loginName: { type: String, required: true, unique: true, minlength: 3, maxlength: 24 },
   displayName: { type: String, required: true, unique: false, minlength: 3, maxlength: 24 },
   password: { type: String, required: true, unique: false },
   emailAddress: { type: String, required: false },
+  goals: { type: [goalSchema], required: false, unique: false},
   joinedDate: { type: Date, default: Date.now },
   lastLoginDate: { type: Date, default: Date.now },
   loginCount: { type: Number, default: 1 },
