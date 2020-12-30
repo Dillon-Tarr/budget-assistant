f0a1df0a7cf340c657695e42585c46b07b76a85d
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-12-30 18:31:13 -0500

    Updated the store to succesfully render when Redux Devtools is not installed

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer'
 
 const initialState = {};
@@ -9,9 +10,8 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(...middleware)
   )
 );
 
